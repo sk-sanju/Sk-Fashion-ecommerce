50becbeb2f8400eeaf85268b81f98b4caa91cf2b
sk-sanju <sanjayskpy1@gmail.com>
2024-03-07 18:20:33 +0530

    added total price in cart

--- fashion/cart/cart.py
+++ fashion/cart/cart.py
@@ -18,6 +18,22 @@ class Cart():
             self.cart[product_id] = int(product_qty)
         self.session.modified = True
         
+    def cart_total(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        quantities = self.cart
+        total = 0
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+
+        return total
+            
     def __len__(self):
         return len(self.cart)
     
